87bcf3d3d54b96489a7cc9472050d0345a187bb8
Swifree <2424493572@qq.com>
2016-06-09 19:36:26 +0800

    添加登录返回结果 返回一个字符串,第一个字母如果是0,则表示登录成功,接下来是跳转的链接

--- src/org/expc/controller/LoginController.java
+++ src/org/expc/controller/LoginController.java
@@ -10,6 +10,7 @@ import org.expc.entity.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("")
@@ -21,14 +22,14 @@ public class LoginController {
 	@Resource(name="adminDaoImpl")
 	private AdminDao adminDao;
 	@RequestMapping("/login")	
-	public String login(String username, String password, String role,
+	@ResponseBody public String login(String username, String password, String role,
 			HttpSession session,Model model)
 	{
 		
 		User user = null;
 		String msg = null;
 		if(null==username||null==password||null==role)
-			msg = "username, password and role must not be empty!";
+			msg = "用户名或密码不能为空";
 		if(role.equals("teacher"))
 			user = (User) teacherDao.get(username);
 		else if(role.equals("admin"))
@@ -36,27 +37,27 @@ public class LoginController {
 		else if(role.equals("student"))
 			user = studentDao.get(username);
 		else {
-			msg = "parameter error! wrong role!";
+			msg = "此身份不存在:"+role;
 		}
 		if(null == user||!user.getPassword().equals(password)) 
 		{
-			msg = "wrong username or password";
+			msg = "错误的用户名或密码";
 		}
-		if(msg!=null)
+		
+		if(msg!=null)      //登录失败
 		{
-			model.addAttribute("msg", msg);
-			model.addAttribute("username",username);
-			model.addAttribute("password",password);
-			model.addAttribute("role", role);
-			return "loginPage";
+			msg="1"+msg;
+			return msg;
 		}
+		//登录成功执行下面的动作
+		msg="0/"+role;
 		session.setAttribute("user", user);
-		return "redirect:/admin";
+		return msg;
 	}
 	@RequestMapping("/logout")
 	public String logout(HttpSession session)
 	{
 		session.removeAttribute("user");
-		return "redirect:loginPage";
+		return "/home";
 	}
 }
